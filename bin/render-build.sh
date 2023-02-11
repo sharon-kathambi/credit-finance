b7367806b0dac86074f37a46ff09709bcb7284d7
Sharon Mwenda <smkathambi@gmail.com>
2023-02-11 20:02:44 +0300

    build command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+#bundle exec rake assets:precompile
+#bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake db:seed
